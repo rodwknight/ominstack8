a698773c00ee7b41747ce1abfacd54a0052983ba
Rodrigo Widmann <rodrigo_widmann@hotmail.com>
2019-08-06 23:56:24 -0300

    instalando mongoose e conectando no banco mongodb usando cluster do atlas

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 const server = express();
+mongoose.connect("mongodb+srv://omnistack:omnistack@cluster0-02goi.mongodb.net/omnistack8?retryWrites=true&w=majority", { useNewUrlParser : true});
 server.use(express.json());
 server.use(routes);
 
